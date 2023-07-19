9b56d4e4ad5d57de6be45881cf83ba73f9985d6c
Juanhen10 <henriquejuan36@gmail.com>
2023-07-18 22:39:24 -0300

    Picture config

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
+# Create your models here.
+
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
+
+
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
 
-# Create your models here.
 
 class Contact(models.Model ):
     first_name = models.CharField(max_length=50)
@@ -18,6 +21,8 @@ class Contact(models.Model ):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'

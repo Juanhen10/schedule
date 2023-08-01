e2d92a8b1ae9e8d62b329c5c617f4b0eb3831121
Juanhen10 <henriquejuan36@gmail.com>
2023-08-01 20:38:11 -0300

    adicionando url para id

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_list_or_404, render
 
 from contact.models import Contact
 
+# Create your views here.
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects\
+    .filter(show=True)\
+    .order_by('-id')[10:20] 
 
     context= {
         'contacts': contacts,
@@ -15,4 +19,17 @@ def index(request):
         'contact/index.html',
         context
     )
-# Create your views here.
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(id=contact_id).last()
+    single_contact = get_list_or_404(Contact, pk=contact_id, show=True)
+    context= {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )

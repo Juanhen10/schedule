f44a33befc40a102da0ab1a45ed78d1c7d1ca769
Juanhen10 <henriquejuan36@gmail.com>
2023-08-15 22:38:10 -0300

    request method post

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,7 +6,12 @@ from contact.models import Contact
 
 
 def create(request):
-
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
     context = {
 
     }

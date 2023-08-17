0ca979046f5121e2f5bb0186e964d2dc03de4511
Juanhen10 <henriquejuan36@gmail.com>
2023-08-16 23:57:48 -0300

    forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
@@ -5,15 +6,30 @@ from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
 
 
+class ContactFrom(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        return super().clean()
+
+
 def create(request):
     if request.method == 'POST':
-        print()
-        print(request.method)
-        print(request.POST.get('first_name'))
-        print(request.POST.get('last_name'))
-        print()
-    context = {
+        context = {
+            'form': ContactFrom(request.POST)
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactFrom()
     }
     return render(
         request,

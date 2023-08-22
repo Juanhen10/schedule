fbbc434888c5bdcc4a3b97aa0eac9f8deed9f360
Juanhen10 <henriquejuan36@gmail.com>
2023-08-21 22:26:21 -0300

    forms

--- contact/forms.py
+++ contact/forms.py
@@ -5,23 +5,48 @@ from contact.models import Contact
 
 
 class ContactFrom(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe a classe b',
+                'placeholder': 'escreva aqui'
+            }
+
+        ),
+        label='Primeiro nome',
+        help_text='Texto de ajuda',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classes-a classe-b',
+        #     'placeholder': 'Escreva aqui',
+        # })
+
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'phone')
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui'})
+
+        # }
 
     def clean(self):
-        cleaned_data = self.cleaned_data
-
         self.add_error(
             'first_name',
             ValidationError(
                 'Mensagem de erro',
-                code = 'ivalid'
+                code='ivalid'
             )
         )
         self.add_error(
             'first_name',
-            ValidationError('Mensagem de erro 2', code= 'invalid')
+            ValidationError('Mensagem de erro 2', code='invalid')
         )
 
         return super().clean()

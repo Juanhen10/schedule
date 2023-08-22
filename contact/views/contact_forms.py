16255ae7c6c8b5238b07f351d8aa33fa7ac0b950
Juanhen10 <henriquejuan36@gmail.com>
2023-08-21 21:14:18 -0300

    forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,17 +3,8 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
+from contact.forms import ContactFrom
 from contact.models import Contact
-
-
-class ContactFrom(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone')
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        return super().clean()
 
 
 def create(request):

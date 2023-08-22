cbfee88deb14d55d49aca8ba7d8640c07ca2ffd5
Juanhen10 <henriquejuan36@gmail.com>
2023-08-21 22:26:16 -0300

    forms

--- contact/models.py
+++ contact/models.py
@@ -10,7 +10,6 @@ from django.utils import timezone
 # category (foreign key), show (boolean), picture (imagem)
 
 
-
 # Depois
 # owner (foreign key)
 class Category(models.Model):
@@ -23,21 +22,23 @@ class Category(models.Model):
     def __str__(self) -> str:
         return f'{self.name} '
 
-class Contact(models.Model ):
+
+class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank= True)
+    last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True)
     owner = models.ForeignKey(
         User,
-        on_delete= models.SET_NULL,
+        on_delete=models.SET_NULL,
         blank=True, null=True
     )
-    
+
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'

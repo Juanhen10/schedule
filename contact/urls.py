41d9d11e7fda3e6a395c5862efc4017b8bb6f4a0
Juanhen10 <henriquejuan36@gmail.com>
2023-08-25 21:08:50 -0300

    forms

--- contact/urls.py
+++ contact/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     # Contact (CRUD)
     path('contact/<int:contact_id>/detail/', views.contact, name = 'contact'),
     path('contact/create/', views.create, name = 'create'),
-    path('contact/<int:contact_id>/update/', views.contact, name = 'contact'),
+    path('contact/<int:contact_id>/update/', views.update, name = 'update'), # type: ignore
     path('contact/<int:contact_id>/delete/', views.contact, name = 'contact'),
 ]
